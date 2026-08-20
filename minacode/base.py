ae0993db829b5ed03525d0245c412b0bd31b5fc6
hit9 <hit9@icloud.com>
2026-08-20 08:46:37 -0700

    Release 0.29.0

--- minacode/base.py
+++ minacode/base.py
@@ -21,7 +21,7 @@ except ImportError:  # pragma: no cover - optional highlighting dependency
     pygments = None
     Token = None  # keep the name defined so class-body/token lookups don't NameError
 
-__version__ = "0.28.0"
+__version__ = "0.29.0"
 
 _ResourceT = TypeVar("_ResourceT")
 
